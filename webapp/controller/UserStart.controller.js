865ca61db3b8d9d320af0efda1440d03a5b4196e
Emanuel-Claudiu Groza <emanuel-claudiu.groza@mhp.com>
2023-08-25 15:21:32 +0300

    connection to database works

--- webapp/controller/UserStart.controller.js
+++ webapp/controller/UserStart.controller.js
@@ -1,15 +1,33 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel, Fragment) {
         "use strict";
 
         return Controller.extend("ratereflect.controller.UserStart", {
             onInit: function () {
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, "CatalogModel");
 
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.getRoute("UserStart").attachPatternMatched(this._onObjectMatched, this);
+            },
+
+            // onAfterRendering: function(){
+            //     this.getView().setBusy(false);
+            // },
+            _onObjectMatched: async function(){
+                var oView = this.getView();
+                oView.setBusy(true);
+                oView.getModel("CatalogModel").refresh();
+                 oView.byId("idProductsTable").getBinding("items").attachEvent("dataReceived", function(oEvent){
+                     this.getView().setBusy(false);
+                 }.bind(this));
             }
         });
     });
