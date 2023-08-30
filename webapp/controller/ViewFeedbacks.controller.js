df94e7dd12274c989da41d710aed87c960fb33da
Carla Vlaiconi <vlaiconi.carla@yahoo.com>
2023-08-30 14:25:09 +0300

    sent and received feedbacks done

--- webapp/controller/ViewFeedbacks.controller.js
+++ webapp/controller/ViewFeedbacks.controller.js
@@ -1,69 +1,44 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel, Filter, FilterOperator) {
+    function (Controller,JSONModel) {
         "use strict";
-        var received= true;
-        var sent = false;
-        var oUserId = 3;
 
         return Controller.extend("ratereflect.controller.ViewFeedbacks", {
-            _onObjectMatched: async function(){
-                var oView = this.getView();
-                // oView.setBusy(true);
-                // oView.getModel("viewModel").refresh();
-                // oView.byId("idProductsTable").getBinding("items").attachEvent("dataReceived", function(oEvent){
-                //     this.getView().setBusy(false);
-                // }.bind(this));
-
-                var oFilter = new sap.ui.model.Filter({
-                    path: "Sender", 
-                    operator: 'EQ', 
-                    value1: oUserId 
-                }); 
-                oView.byId("idProductsTableSent").getBinding("items").filter([oFilter]);
-
-                var oFilter2 = new sap.ui.model.Filter({
-                    path: "Receive", 
-                    operator: 'EQ', 
-                    value1: oUserId 
-                }); 
-                oView.byId("idProductsTableReceived").getBinding("items").filter([oFilter2]);
-
-
-                oView.getModel().refresh();
-
-
-            },
+           
             onInit: function () {
             // Creating a JSON model for your view
             var oViewModel = new JSONModel();
             this.getView().setModel(oViewModel, "viewModel");
-            // Creating an OData model
-            var oODataModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/Z_RATEREFLECT_SRV/");
-            // Applying filter
-            //var oUserId = sap.ui.getCore().getModel("userDetailsModel").getData()[0].User_ID;
-            //var oUserId = 3;
-            // Fetching filtered data
-            // oODataModel.read("/FeedbacksSet", {
-            //     filters: [oFilter],
-            //     success: function (oData, oResponse) {
-            //         // Populate the JSON model with filtered data
-            //         oViewModel.setData({ FeedbacksSet: oData.results });
-            //         //oView.setBusy(false);
-            //     },
-            //     error: function (oError) {
-            //         // Handle errors
-            //     }
-            //});
-                var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.getRoute("ViewFeedbacks").attachPatternMatched(this._onObjectMatched, this);
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("ViewFeedbacks").attachPatternMatched(this._onObjectMatched, this);
+                
+            },
+            _onObjectMatched: async function(){
+                var oView = this.getView();
+
+                var oFilter = new sap.ui.model.Filter({
+                    path: "Sender", 
+                    operator: 'EQ', 
+                    value1: sap.ui.getCore().getModel("userDetailsModel").getData()[0].User_ID 
+
+                }); 
+                oView.byId("idProductsTableSent").getBinding("items").filter([oFilter]);
+
+                var oFilter2 = new sap.ui.model.Filter({
+                    path: "Receiver", 
+                    operator: 'EQ', 
+                    value1: sap.ui.getCore().getModel("userDetailsModel").getData()[0].User_ID 
+                }); 
+                oView.byId("idProductsTableReceived").getBinding("items").filter([oFilter2]);
+                oView.getModel().refresh();
+
+
             },
 
             onReceivedPress: function () {
